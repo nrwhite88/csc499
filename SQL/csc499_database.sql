7b5313bb186d8c67757fd2a8788a93dd9bca553f
Nick White <nickfone@gmail.com>
2019-10-08 16:24:53 -0400

    3nf revisions to db design

--- SQL/csc499_database.sql
+++ SQL/csc499_database.sql
@@ -19,26 +19,9 @@ CREATE TABLE Users   (
 	PRIMARY KEY(user_ID)
 
 );
-CREATE TABLE Bookings	(
-
-	booking_ID INT NOT NULL AUTO_INCREMENT,
-	bar_ID INT NOT NULL,
-    band_ID INT NOT NULL,
-	datetime_requested DATETIME,
-	duration_hrs TINYINT,
-	confirmed BOOLEAN,
-	datetime_of_request DATETIME,
-
-	PRIMARY KEY(booking_ID),
-	FOREIGN KEY(bar_ID) REFERENCES Users(user_ID),
-	FOREIGN KEY(band_ID) REFERENCES Users(user_ID)
-
-);
 CREATE TABLE Reviews    (
 
     review_ID INT NOT NULL AUTO_INCREMENT,
-    to_ID INT NOT NULL,
-    from_ID INT NOT NULL,
     rating TINYINT,
     review_text VARCHAR(250),
     review_datetime DATETIME,
@@ -48,17 +31,51 @@ CREATE TABLE Reviews    (
     FOREIGN KEY(to_ID) REFERENCES Users(user_ID)
     
 );
+CREATE TABLE SentReviews    (
+
+    review_ID INT NOT NULL,
+    sender_ID INT NOT NULL,
+    
+    CONSTRAINT sent_review PRIMARY KEY(review_ID, sender_ID)
+    FOREIGN KEY(review_ID) REFERENCES Reviews(review_ID),
+    FOREIGN KEY(sender_ID) REFERENCES Users(user_ID)
+);
+CREATE TABLE ReceivedReviews    (
+
+    review_ID INT NOT NULL,
+    receiver_ID INT NOT NULL,
+    
+    CONSTRAINT receiver_review PRIMARY KEY(review_ID, receiver_ID)
+    FOREIGN KEY(review_ID) REFERENCES Reviews(review_ID),
+    FOREIGN KEY(receiver_ID) REFERENCES Users(receiver_ID)
+);
 CREATE TABLE Messages   (
 
     message_ID INT NOT NULL AUTO_INCREMENT,
-    to_ID INT NOT NULL,
-    from_ID INT NOT NULL,
     message_text VARCHAR(500),
     message_datetime DATETIME,
 
     PRIMARY KEY(message_ID),
-    FOREIGN KEY(from_ID) REFERENCES Users(user_ID),
-    FOREIGN KEY(to_ID) REFERENCES Users(user_ID)
+    
+);
+CREATE TABLE SentMessages   (
+
+    message_ID INT NOT NULL,
+    sender_ID INT NOT NULL,
+    
+    CONSTRAINT sent_message PRIMARY KEY(message_ID, sender_ID)
+    FOREIGN KEY(message_ID) REFERENCES Messages(message_ID),
+    FOREIGN KEY(sender_ID) REFERENCES Users(user_ID)
+    
+);
+CREATE TABLE ReceivedMessages   (
+
+    message_ID INT NOT NULL,
+    receiver_ID INT NOT NULL,
+    
+    CONSTRAINT received_message PRIMARY KEY(message_ID, sender_ID)
+    FOREIGN KEY(message_ID) REFERENCES Messages(message_ID),
+    FOREIGN KEY(receiver_ID) REFERENCES Users(user_ID)
     
 );
 CREATE TABLE Followings (
@@ -71,26 +88,103 @@ CREATE TABLE Followings (
     FOREIGN KEY(being_followed_ID) REFERENCES Users(user_ID)
  
 );
+CREATE TABLE Flags  (
+
+    flag_ID INT NOT NULL AUTO_INCREMENT,
+    flag_reason VARCHAR(100),
+    
+    PRIMARY KEY(flag_ID),
+
+);
+CREATE TABLE SentFlags  (
+
+    flag_ID INT NOT NULL,
+    sender_ID INT NOT NULL,
+
+    PRIMARY KEY(flag_ID, sender_ID),
+    FOREIGN KEY(flag_ID) REFERENCES Flags(flag_ID),
+    FOREIGN KEY(sender_ID) REFERENCES Users(user_ID)
+);
+CREATE TABLE ReceivedFlags  (
+
+    flag_ID INT NOT NULL,
+    receiver_ID INT NOT NULL,
+
+    PRIMARY KEY(flag_ID, receiver_ID),
+    FOREIGN KEY(flag_ID) REFERENCES Flags(flag_ID),
+    FOREIGN KEY(receiver_ID) REFERENCES Users(user_ID)
+);
+CREATE TABLE Disciplines(
+
+    discipline_ID INT NOT NULL AUTO_INCREMENT,
+    discipline_type TINYINT NOT NULL,
+    discipline_datetime DATETIME,
+    discipline_duration SMALLINT,
+    
+    PRIMARY KEY(discipline_ID),
+    FOREIGN KEY(admin_ID) REFERENCES Users(user_ID),
+    FOREIGN KEY(flag_ID) REFERENCES Flags(flag_ID)
+
+);
+CREATE TABLE FlagDisciplines(
+
+    discipline_ID INT NOT NULL,
+    flag_ID INT NOT NULL,
+
+    CONSTRAINT received_discipline PRIMARY KEY(discipline_ID, flag_ID),
+    FOREIGN KEY(discipline_ID) REFERENCES Disciplines(discipline_ID),
+    FOREIGN KEY(flag_ID) REFERENCES Flags(flag_ID)
+
+);
+CREATE TABLE AdministeredDisciplines(
+
+    discipline_ID INT NOT NULL,
+    admin_ID INT NOT NULL,
+    
+    CONSTRAINT administered_discipline PRIMARY KEY(discipline_ID, admin_ID),
+    FOREIGN KEY(discipline_ID) REFERENCES Disciplines(discipline_ID),
+    FOREIGN KEY(admin_ID) REFERENCES Users(user_ID)
+    
+);
+CREATE TABLE Bookings	(
+
+	booking_ID INT NOT NULL AUTO_INCREMENT,
+	requested_datetime DATETIME,
+	duration_hrs TINYINT,
+	confirmed BOOLEAN,
+	datetime_of_request DATETIME,
+
+	PRIMARY KEY(booking_ID),
+	FOREIGN KEY(bar_ID) REFERENCES Users(user_ID),
+	FOREIGN KEY(band_ID) REFERENCES Users(user_ID)
+
+);
 CREATE TABLE Shows  (
 
     show_ID INT NOT NULL,
-    bar_ID INT NOT NULL,
     start_datetime DATETIME,
     end_datetime DATETIME,
     
     PRIMARY KEY(show_ID),
-    FOREIGN KEY(bar_ID) REFERENCES Users(user_ID)
+
+);
+CREATE TABLE ShowBookings   (
+
+    show_ID INT NOT NULL,
+    booking_ID INT NOT NULL,
+    
+    CONSTRAINT show_booking PRIMARY KEY(show_ID, booking_ID)
+    FOREIGN KEY(show_ID) REFERENCES Shows(show_ID),
+    FOREIGN KEY(booking_ID) REFERENCES Bookings(booking_ID),
 
 );
 CREATE TABLE Tours  (
 
     tour_ID INT NOT NULL AUTO_INCREMENT,
-    band_ID INT NOT NULL,
     start_datetime DATETIME,
     end_datetime DATETIME,
     
     PRIMARY KEY(tour_ID),
-    FOREIGN KEY(band_ID) REFERENCES Users(user_ID)
 
 );
 CREATE TABLE TourStops  (
@@ -98,9 +192,9 @@ CREATE TABLE TourStops  (
     tour_ID INT NOT NULL,
     show_ID INT NOT NULL,
     
-    CONSTRAINT tour_stop PRIMARY KEY(tour_ID, bar_ID),
+    CONSTRAINT tour_stop PRIMARY KEY(tour_ID, show_ID),
     FOREIGN KEY(tour_ID) REFERENCES Tours(tour_ID),
-    FOREIGN KEY(show_ID) REFERENCES Users(user_ID)
+    FOREIGN KEY(show_ID) REFERENCES Shows(show_ID)
 
 );
 CREATE TABLE Attendance (
@@ -112,30 +206,4 @@ CREATE TABLE Attendance (
     FOREIGN KEY(fan_ID) REFERENCES Users(user_ID),
     FOREIGN KEY(show_ID) REFERENCES Shows(show_ID)
     
-);
-CREATE TABLE Flags  (
-
-    flag_ID INT NOT NULL AUTO_INCREMENT,
-    flagger_ID INT NOT NULL,
-    flagged_message_ID INT,
-    flag_reason VARCHAR(100),
-    
-    PRIMARY KEY(flag_ID),
-    FOREIGN KEY(flagger_ID) REFERENCES Users(user_ID),
-    FOREIGN KEY(flagged_message_ID) REFERENCES Messages(message_ID)
-
-);
-CREATE TABLE Disciplines(
-
-    flag_ID INT NOT NULL,
-    offender_ID INT NOT NULL,
-    admin_ID INT NOT NULL,
-    discipline_type TINYINT NOT NULL,
-    discipline_datetime DATETIME,
-    discipline_duration SMALLINT,
-    
-    CONSTRAINTdiscipline PRIMARY KEY(flag_ID, offender_ID),
-    FOREIGN KEY(user_ID) REFERENCES Users(user_ID),
-    FOREIGN KEY(flag_ID) REFERENCES Flags(flag_ID)
-
 );
